150213c964b1c505295227f1af8033ee54b9fdf6
jwatmore <jason.watmore@hotmail.com>
2019-05-17 10:10:05 +1000

    added part 4

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ A video tutorial series showing how to build an Angular 7 application step by st
 
 * [Part 1 (Windows) - Setup Development Machine on Windows](https://www.youtube.com/watch?v=FNYsvgmblM0)
 * [Part 1 (Mac) - Setup Development Machine on Mac](https://www.youtube.com/watch?v=NLsey2LlJqo)
-* [Part 2 - Create Base Project Structure & Webpack Config](http://jasonwatmore.com/post/2019/04/24/angular-7-tutorial-part-2-create-base-project-structure-webpack-config)
-* [Part 3 - Add Routing & Multiple Pages](http://jasonwatmore.com/post/2019/04/29/angular-7-tutorial-part-3-add-routing-multiple-pages)
+* [Part 2 - Create Base Project Structure & Webpack Config](https://jasonwatmore.com/post/2019/04/24/angular-7-tutorial-part-2-create-base-project-structure-webpack-config)
+* [Part 3 - Add Routing & Multiple Pages](https://jasonwatmore.com/post/2019/04/29/angular-7-tutorial-part-3-add-routing-multiple-pages)
+* [Part 4 - Login Form, Authentication Service & Route Guard](https://jasonwatmore.com/post/2019/05/17/angular-7-tutorial-part-4-login-form-authentication-service-route-guard)
 
 Next parts coming soon!
 
-* Part 4 - Login Form, Authentication Service & Route Guard
 * Part 5 - Registration Form & User Service
 * Part 6 - Home Page & Alert Component
 * Part 7 (Optional) - Migrating to an Angular CLI Project
---
+++ part-4/src/app/_helpers/fake-backend.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpResponse, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { delay, mergeMap, materialize, dematerialize } from 'rxjs/operators';
+
+let users = [{ id: 1, firstName: 'Jason', lastName: 'Watmore', username: 'test', password: 'test' }];
+
+@Injectable()
+export class FakeBackendInterceptor implements HttpInterceptor {
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        const { url, method, headers, body } = request;
+
+        // wrap in delayed observable to simulate server api call
+        return of(null)
+            .pipe(mergeMap(handleRoute))
+            .pipe(materialize()) // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648)
+            .pipe(delay(500))
+            .pipe(dematerialize());
+
+        function handleRoute() {
+            switch (true) {
+                case url.endsWith('/users/authenticate') && method === 'POST':
+                    return authenticate();
+                default:
+                    // pass through any requests not handled above
+                    return next.handle(request);
+            }    
+        }
+
+        // route functions
+
+        function authenticate() {
+            const { username, password } = body;
+            const user = users.find(x => x.username === username && x.password === password);
+            if (!user) return error('Username or password is incorrect');
+            return ok({
+                id: user.id,
+                username: user.username,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                token: 'fake-jwt-token'
+            })
+        }
+
+        // helper functions
+
+        function ok(body?) {
+            return of(new HttpResponse({ status: 200, body }))
+        }
+
+        function error(message) {
+            return throwError({ error: { message } });
+        }
+    }
+}
+
+export const fakeBackendProvider = {
+    // use fake backend in place of Http service for backend-less development
+    provide: HTTP_INTERCEPTORS,
+    useClass: FakeBackendInterceptor,
+    multi: true
+};
